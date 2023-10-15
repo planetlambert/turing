445fe8a81f16689957430059380c31e2b6afe77e
Justin Kennedy <justin@justinkennedy.com>
2023-10-15 11:27:01 -0400

    Implement machine that tests for well-definedness

--- diagonal.go
+++ diagonal.go
@@ -1,6 +1,82 @@
 package turing
 
-// TODO: Create `W` which tests if a machine is well-defined
+// The following m-functions and m-configurations test if a Standard Description
+// on the Tape is well-defined. It is assumed that the head of the Tape is at the
+// start of the S.D.
+var (
+	possibleSymbolsForWellDefinedMachine = []string{
+		string(a),
+		string(c),
+		string(d),
+		string(l),
+		string(r),
+		string(n),
+		string(semicolon),
+		"s",
+		"u",
+	}
+
+	wellDefined = []MConfiguration{
+		{"b", []string{"*", " "}, []string{}, "checkSemicolon"},
+	}
+
+	satisfactory = []MConfiguration{
+		{"satisfactory", []string{"*", " "}, []string{}, "decide(s)"},
+	}
+
+	unsatisfactory = []MConfiguration{
+		{"unsatisfactory", []string{"*", " "}, []string{}, "decide(u)"},
+	}
+
+	decide = []MConfiguration{
+		{"decide(d)", []string{"*"}, []string{"R", "R"}, "decide(d)"},
+		{"decide(d)", []string{" "}, []string{"L", "L"}, "decide1(d)"},
+		{"decide1(d)", []string{"*"}, []string{"E", "L", "L"}, "decide(d)"},
+		{"decide1(d)", []string{" "}, []string{"Pd"}, "halt"},
+	}
+
+	checkSemiColon = []MConfiguration{
+		{"checkSemicolon", []string{";"}, []string{"R", "R"}, "checkName"},
+		{"checkSemicolon", []string{"!;"}, []string{}, "unsatisfactory"},
+		{"checkSemicolon", []string{" "}, []string{"L", "L"}, "checkSemicolon1"},
+		{"checkSemicolon1", []string{" "}, []string{}, "unsatisfactory"},
+		{"checkSemicolon1", []string{"*"}, []string{"R", "R"}, "satisfactory"},
+	}
+
+	checkName = []MConfiguration{
+		{"checkName", []string{"D"}, []string{"R", "R"}, "checkName1"},
+		{"checkName", []string{"!D", " "}, []string{}, "unsatisfactory"},
+		{"checkName1", []string{"A"}, []string{"R", "R"}, "checkName1"},
+		{"checkName1", []string{"!A", " "}, []string{}, "checkSymbol"},
+	}
+
+	checkSymbol = []MConfiguration{
+		{"checkSymbol", []string{"D"}, []string{"R", "R"}, "checkSymbol1"},
+		{"checkSymbol", []string{"!D", " "}, []string{}, "unsatisfactory"},
+		{"checkSymbol1", []string{"C"}, []string{"R", "R"}, "checkSymbol1"},
+		{"checkSymbol1", []string{"!C", " "}, []string{}, "checkPrintOp"},
+	}
+
+	checkPrintOp = []MConfiguration{
+		{"checkPrintOp", []string{"D"}, []string{"R", "R"}, "checkPrintOp1"},
+		{"checkPrintOp", []string{"!D", " "}, []string{}, "unsatisfactory"},
+		{"checkPrintOp1", []string{"C"}, []string{"R", "R"}, "checkPrintOp1"},
+		{"checkPrintOp1", []string{"!C", " "}, []string{}, "checkMoveOp"},
+	}
+
+	checkMoveOp = []MConfiguration{
+		{"checkMoveOp", []string{"L", "R", "N"}, []string{"R", "R"}, "checkFinalMConfig"},
+		{"checkMoveOp", []string{"!L", "!R", "!N", " "}, []string{}, "unsatisfactory"},
+	}
+
+	checkFinalMConfig = []MConfiguration{
+		{"checkFinalMConfig", []string{"D"}, []string{"R", "R"}, "checkFinalMConfig1"},
+		{"checkFinalMConfig", []string{"!D", " "}, []string{}, "unsatisfactory"},
+		{"checkFinalMConfig1", []string{"A"}, []string{"R", "R"}, "checkFinalMConfig1"},
+		{"checkFinalMConfig1", []string{"!A", " "}, []string{}, "checkSemicolon"},
+	}
+)
+
 // TODO: Create non-`D` part of `H`
 
 // TODO: Implement `M1`, `M2`, etc.
--- diagonal_test.go
+++ diagonal_test.go
@@ -1,6 +1,7 @@
 package turing
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -22,7 +23,45 @@ func TestFirstCircleFreeDN(t *testing.T) {
 	checkTape(t, m.TapeString(), "S1S1S1S1S1S1S1")
 }
 
-// TODO: Test `W` (test it on the above). If it doesn't take too long, show `313,325,317` is the first well-defined.
+func TestWellDefinedness(t *testing.T) {
+	mConfigurations := []MConfiguration{}
+	mConfigurations = append(mConfigurations, wellDefined...)
+	mConfigurations = append(mConfigurations, satisfactory...)
+	mConfigurations = append(mConfigurations, unsatisfactory...)
+	mConfigurations = append(mConfigurations, decide...)
+	mConfigurations = append(mConfigurations, checkSemiColon...)
+	mConfigurations = append(mConfigurations, checkName...)
+	mConfigurations = append(mConfigurations, checkSymbol...)
+	mConfigurations = append(mConfigurations, checkPrintOp...)
+	mConfigurations = append(mConfigurations, checkMoveOp...)
+	mConfigurations = append(mConfigurations, checkFinalMConfig...)
+
+	m := NewMachine(NewAbbreviatedTable(AbbreviatedTableInput{
+		MConfigurations:        mConfigurations,
+		Tape:                   strings.Split("; D A D A D A D", ""),
+		StartingMConfiguration: "b",
+		PossibleSymbols:        possibleSymbolsForWellDefinedMachine,
+	}))
+
+	m.MoveN(100000)
+	if m.TapeString()[0] != 'u' {
+		t.Errorf("expecting unsatisfactory but got %s", m.TapeString())
+	}
+
+	m = NewMachine(NewAbbreviatedTable(AbbreviatedTableInput{
+		MConfigurations:        mConfigurations,
+		Tape:                   strings.Split("; D A D D C R D A", ""),
+		StartingMConfiguration: "b",
+		PossibleSymbols:        possibleSymbolsForWellDefinedMachine,
+		Debug:                  true,
+	}))
+
+	m.MoveN(100000)
+	if m.TapeString()[0] != 's' {
+		t.Errorf("expecting satisfactory but got %s", m.TapeString())
+	}
+}
+
 // TODO: Test non-`D` part of `H`
 
 // TODO: Test `M1`, `M2`, etc.
